f0e3a04b60aca34430fc8a47e1d79ea0e9f44e8d
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-02 16:17:26 +0300

    Write and WriteLine

--- CSharp_level1/Program.cs
+++ CSharp_level1/Program.cs
@@ -10,15 +10,26 @@ namespace CSharp_level1
     {
         static void Output()
         {
+            Console.WriteLine("========================WriteLine=========================");
             Console.WriteLine("Hello World!");
             Console.WriteLine("My Name is Ahmed Mady");
             Console.WriteLine("I am Learning C#");
             Console.WriteLine("It is an easy language to learn :-)");
             Console.WriteLine("The sum of 30 + 20 is " + (30 + 20));
-            Console.WriteLine("The sum of 30 + 20 is " + 30 + 20+ " without brackets");
+            Console.WriteLine("The sum of 30 + 20 is " + 30 + 20 + " without brackets");
+            Console.WriteLine("=========================Write=============================");
+            Console.Write("Hello World!");
+            Console.Write("My Name is Ahmed Mady");
+            Console.Write("I am Learning C#");
+            Console.Write("It is an easy language to learn :-)");
+            Console.WriteLine("\n==========================================================");
             Console.ReadKey();
 
         }
+
+
+
+
         static void Main(string[] args)
         {
             //Console.WriteLine("hi, ahmed mady");
