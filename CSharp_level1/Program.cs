b8aee1e14e6f13dae7bc2ca392779f54366eed22
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-02 16:29:41 +0300

    EscapeCharacters

--- CSharp_level1/Program.cs
+++ CSharp_level1/Program.cs
@@ -26,13 +26,52 @@ namespace CSharp_level1
             Console.ReadKey();
 
         }
-
         static void FormattedString()
         {
+            Console.WriteLine("Welcome to {0}", "Egypt");
             Console.WriteLine("Hi, My Name Is {0} And I Live In {1}.", "Ahmed Mady", "Egypt");
             Console.ReadLine();
         }
+        static void EscapeCharacters()
+        {
+            Console.WriteLine("Useful Escape Characters:\n");
 
+            //Newline
+            Console.WriteLine("Newline:");
+            Console.WriteLine("Welcome to \n Egypt\n");
+
+            //Tab
+            Console.WriteLine("Tab:");
+            Console.WriteLine("Welcome to\tEgypt\n");
+
+            //Backspace
+            Console.WriteLine("Backspace:");
+            Console.WriteLine("Welcome to \bEgypt\n");
+
+            //Single quote
+            Console.WriteLine("Single Quote:");
+            Console.WriteLine("I\'m Ahmed Mady.\n");
+
+            //Double quote
+            Console.WriteLine("Double Quote:");
+            Console.WriteLine("Welcome to \"Egypt\"\n");
+
+            //Backslash
+            Console.WriteLine("Backslash:");
+            Console.WriteLine("Welcome to \\ Egypt\\\n");
+
+            //Alert
+            Console.WriteLine("Alert:");
+            Console.WriteLine("\a");
+
+
+            Console.ReadKey();
+        }
+        //static void Test() { Console.ReadLine(); }
+        //static void Test() { Console.ReadLine(); }
+        //static void Test() { Console.ReadLine(); }
+        //static void Test() { Console.ReadLine(); }
+        //static void Test() { Console.ReadLine(); }
 
         static void Main(string[] args)
         {
@@ -40,7 +79,8 @@ namespace CSharp_level1
             //Console.ReadKey();
             /////////////////////////////////////////////
             //Output();
-            FormattedString();
+            //FormattedString();
+            EscapeCharacters();
         }
     }
 }
