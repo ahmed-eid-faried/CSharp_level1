6331f4549019a11ced3af8041985f6b15bb80fad
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-02 16:13:32 +0300

    Output

--- CSharp_level1/Program.cs
+++ CSharp_level1/Program.cs
@@ -8,10 +8,22 @@ namespace CSharp_level1
 {
     internal class Program
     {
+        static void Output()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My Name is Ahmed Mady");
+            Console.WriteLine("I am Learning C#");
+            Console.WriteLine("It is an easy language to learn :-)");
+            Console.WriteLine("The sum of 30 + 20 is " + (30 + 20));
+            Console.WriteLine("The sum of 30 + 20 is " + 30 + 20+ " without brackets");
+            Console.ReadKey();
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("hi, ahmed mady");
-            Console.ReadKey();
+            //Console.WriteLine("hi, ahmed mady");
+            //Console.ReadKey();
+            Output();
         }
     }
 }
