d8eecf0ea1699857f74e671e46c344251282fe24
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-04 00:21:31 +0300

    OperatorsDateTime

--- CSharp_level1/mydatatime.cs
+++ CSharp_level1/mydatatime.cs
@@ -140,8 +140,9 @@ namespace CSharp_level1
             DateTime dt1 = new DateTime(2015, 12, 20);
             DateTime dt2 = new DateTime(2016, 12, 31, 5, 10, 20);
             TimeSpan time = new TimeSpan(10, 5, 25, 50);
-
+            //DateTime + TimeSpan
             Console.WriteLine(dt2 + time); // 1/10/2017 10:36:10 AM
+            Console.WriteLine(dt2 - time); // 20/12/2016 11:44:30
             Console.WriteLine(dt2 - dt1); //377.05:10:20
             Console.WriteLine(dt1 == dt2); //False
             Console.WriteLine(dt1 != dt2); //True
@@ -149,7 +150,6 @@ namespace CSharp_level1
             Console.WriteLine(dt1 < dt2); //True
             Console.WriteLine(dt1 >= dt2); //False
             Console.WriteLine(dt1 <= dt2);//True
-
 
             Console.ReadKey();
         }
