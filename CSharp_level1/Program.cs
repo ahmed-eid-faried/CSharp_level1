e9cb196c31335cbb505f7564fcd09ddf4cb94c63
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-03 11:00:30 +0300

    Enums

--- CSharp_level1/Program.cs
+++ CSharp_level1/Program.cs
@@ -237,12 +237,49 @@ namespace CSharp_level1
             bool b2 = default;// false
             char c2 = default;// '\0'
 
-            int j;
-            Console.WriteLine("{0} , {1} , {2} , {3} , {4} , {5} , {6} , {7} , {8} , {9} , {10}",
-                i, f, d, b, c, i2, f2, d2, b2, c2, j);
+            Console.WriteLine("{0} , {1} , {2} , {3} , {4} , {5} , {6} , {7} , {8} , {9} ",
+               i, f, d, b, c, i2, f2, d2, b2, c2);
             Console.ReadKey();
         }
-        //static void Test() { Console.ReadLine(); }
+        enum enWeekDays
+        {
+            Monday,     // 0
+            Tuesday,    // 1
+            Wednesday,  // 2
+            Thursday,   // 3
+            Friday,     // 4
+            Saturday,   // 5
+            Sunday      // 6
+        }
+        //if you set a value , it will auto number everything after it.
+        enum enCategories
+        {
+            Electronics,    // 0
+            Food,           // 1
+            Automotive = 6, // 6
+            Arts,           // 7
+            BeautyCare,     // 8
+            Fashion         // 9
+        }
+        //Enum can have any numarical data type byte, sbyte, short, ushort, int, uint, long, or ulong
+        //but not string
+        enum enCategories2 : byte
+        {
+            Electronics = 1,
+            Food = 5,
+            Automotive = 6,
+            Arts = 10,
+            BeautyCare = 11,
+            Fashion = 15
+        }
+        static void Enums()
+        {
+            enWeekDays WeekDays;
+
+            WeekDays = enWeekDays.Friday;
+            Console.WriteLine(WeekDays);
+            Console.ReadKey();
+        }
         //static void Test() { Console.ReadLine(); }
         //static void Test() { Console.ReadLine(); }
         //static void Test() { Console.ReadLine(); }
@@ -264,7 +301,8 @@ namespace CSharp_level1
             //Vairables();
             //ImplicitlyTypedVariables();
             //NumbersDatatypes();
-            DefaultValues();
+            //DefaultValues();
+            Enums();
         }
     }
 }
