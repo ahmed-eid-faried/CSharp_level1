1ccecf487b9037a05ce7edbc0b0e9fa7633841ce
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-03 12:15:23 +0300

    struct

--- CSharp_level1/Program.cs
+++ CSharp_level1/Program.cs
@@ -348,7 +348,37 @@ namespace CSharp_level1
 
             Console.ReadKey();
         }
-        //static void Test() { Console.ReadLine(); }
+        struct stStudent
+        {
+            public string FirstName;
+            public string LastName;
+        }
+
+        static void @struct()
+        { //A struct object can be created with or without the new operator,
+            //same as primitive type variables.
+
+            stStudent Student;
+            stStudent Student2 = new stStudent();
+
+
+            Student.FirstName = "Mohammed";
+            Student.LastName = "Abu-Hadhoud";
+
+
+            Console.WriteLine(Student.FirstName);
+            Console.WriteLine(Student.LastName);
+
+
+            Student2.FirstName = "Ali";
+            Student2.LastName = "Ahmed";
+
+
+            Console.WriteLine(Student2.FirstName);
+            Console.WriteLine(Student2.LastName);
+
+            Console.ReadKey();
+        }
         //static void Test() { Console.ReadLine(); }
         //static void Test() { Console.ReadLine(); }
         //static void Test() { Console.ReadLine(); }
@@ -370,7 +400,8 @@ namespace CSharp_level1
             //DefaultValues();
             //Enums();
             //NullableEx();
-            AnonymousType();
+            //AnonymousType();
+            @struct();
         }
     }
 }
