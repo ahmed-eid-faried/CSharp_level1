25447be274ffb66660d2379883dfaf90dcd5213d
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-04 13:33:41 +0300

    IfElseIfAndNestedIfStatement

--- CSharp_level1/mydatatime.cs
+++ CSharp_level1/mydatatime.cs
@@ -614,7 +614,50 @@ namespace CSharp_level1
             Console.ReadLine();
         }
 
-        //public static void Test() { Console.ReadLine(); }
+    }
+    internal class ControlFlow
+    {
+        public static void IfElseIfAndNestedIfStatement()
+        {
+            int x = 10; int y = 20;
+
+            //if then statement
+            if (x == 10 && y <= 20)
+            {
+                Console.WriteLine("yes x = 10 and y<=20");
+            }
+
+
+
+            //if then else statement
+            if (x == 11)
+            {
+                Console.WriteLine("yes x = 11 ");
+            }
+            else
+            {
+                Console.WriteLine("yes x != 11 ");
+            }
+
+
+            //if else if statement
+            int number = 12;
+            if (number < 5)
+            {
+                Console.WriteLine("{0} is less than 5", number);
+            }
+            else if (number > 5)
+            {
+                Console.WriteLine("{0} is greater than 5", number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is equal to 5");
+            }
+
+
+            Console.ReadKey();
+        }
         //public static void Test() { Console.ReadLine(); }
         //public static void Test() { Console.ReadLine(); }
     }
