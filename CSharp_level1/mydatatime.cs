45da1b7d8a0ea2b478485a0c30df303de5931b86
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-04 13:44:26 +0300

    SimpleCalculator

--- CSharp_level1/mydatatime.cs
+++ CSharp_level1/mydatatime.cs
@@ -707,7 +707,50 @@ namespace CSharp_level1
 
             Console.ReadKey();
         }
-        //public static void Test() { Console.ReadLine(); }
+        public static void SimpleCalculator()
+        {
+            //Simple Calculator
+
+            char op;
+            double first, second, result;
+
+            Console.Write("Enter first number: ");
+            first = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            second = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter operator (+, -, *, /): ");
+            op = (char)Console.Read();
+
+            switch (op)
+            {
+                case '+':
+                    result = first + second;
+                    Console.WriteLine("{0} + {1} = {2}", first, second, result);
+                    break;
+
+                case '-':
+                    result = first - second;
+                    Console.WriteLine("{0} - {1} = {2}", first, second, result);
+                    break;
+
+                case '*':
+                    result = first * second;
+                    Console.WriteLine("{0} * {1} = {2}", first, second, result);
+                    break;
+
+                case '/':
+                    result = first / second;
+                    Console.WriteLine("{0} / {1} = {2}", first, second, result);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Operator");
+                    break;
+
+            }
+
+            Console.ReadKey();
+        }
         //public static void Test() { Console.ReadLine(); }
         //public static void Test() { Console.ReadLine(); }
         //public static void Test() { Console.ReadLine(); }
